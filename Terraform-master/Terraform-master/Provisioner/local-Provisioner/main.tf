499acfe4e914b7d745a7bfd2a445409fd9197d8e
E Njang <104914526+Ekangaki@users.noreply.github.com>
2023-12-26 11:28:45 -0500

    Update main.tf

--- Terraform-master/Terraform-master/Provisioner/local-Provisioner/main.tf
+++ Terraform-master/Terraform-master/Provisioner/local-Provisioner/main.tf
@@ -4,7 +4,7 @@ region = "us-east-1"
 
 
 resource "aws_instance" "web" {
-  ami           = "ami-04bf6dcdc9ab498ca"
+  ami           = "ami-0230bd60aa48260c6"
   instance_type = "t2.micro"
   tags = {
     Name = "local-exec-provisioner"
